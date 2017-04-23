42d574d62de62da565e6d4115d9a4da886135924
1461489 <1461489@gmail.com>
2017-04-23 17:34:35 +0700

    them TeamPlayer cho Addmin

---
+++ WEB2_GK_demo/WEB2_GK_demo/Areas/Admin/Controllers/AdminNSXController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB2_GK_demo.Areas.Admin.Controllers
+{
+    public class AdminNSXController : Controller
+    {
+        // GET: Admin/AdminNSX
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/AdminNSX/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/AdminNSX/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminNSX/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AdminNSX/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminNSX/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AdminNSX/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminNSX/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WEB2_GK_demo/WEB2_GK_demo/Areas/Admin/Controllers/AdminSPController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB2_GK_demo.Areas.Admin.Controllers
+{
+    public class AdminSPController : Controller
+    {
+        // GET: Admin/AdminSP
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/AdminSP/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/AdminSP/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminSP/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AdminSP/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminSP/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/AdminSP/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminSP/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
